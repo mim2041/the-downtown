9970224d482f5a97a27de0e265e90343091805f6
mim2041 <suraiyamim338@gmail.com>
2026-01-20 19:38:26 +0600

    Project initialized

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import HomePage from "./_Home/page";
+
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-neutral-100 max-w-[1600px] mx-auto">
+      <HomePage />
+    </main>
+  );
+}
+
